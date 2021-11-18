69bd35ff7ddccc73c448f985a81e979633a3da16
Kevin CHALLIT <kevin.challit@gmail.com>
2021-11-18 23:09:52 +0100

    finish index page

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -4,6 +4,12 @@ import '../styles/accueil.css';
 import logo from '../images/logo.svg';
 import general from '../images/general.jpg';
 import health from '../images/health.jpg';
+import technology from '../images/technology.jpg'
+import business from '../images/business.jpg'
+import entertainment from '../images/entertainment.jpg'
+import science from '../images/science.jpg'
+import sports from '../images/sports.jpg'
+import { Link } from 'react-router-dom';
 
 const Accueil = () => {
     return (
@@ -16,20 +22,44 @@ const Accueil = () => {
             <h2>L'information à chaud !</h2>
         </div>
     </div>
+    
     <div className="category-container">
+
         <div className="category-list_container">
-            <a href="#" id="btn-category">
-                <img className="category-list_img" src={general} />
-                <span className="category-list_link">Général'</span>
-            </a>
-        </div>
+            <Link to="/general" >
+                 <img className="category-list_img" src={general}/>
+                <span className="category-list_link">Général</span>
+            </Link>    
+            <Link to="/business" >
+                <img className="category-list_img" src={business}/>
+                <span className="category-list_link">Economie</span>
+            </Link>    
+            <Link to="/sports" >
+                <img className="category-list_img" src={sports}/>
+                <span className="category-list_link">Sport</span>
+            </Link> 
+        </div> 
+
+        <div className="category-list_container">
+            <Link to="/entertainment" >
+                <img className="category-list_img" src={entertainment}/>
+                <span className="category-list_link">People</span>
+            </Link>    
+            <Link to="/health" >
+                <img className="category-list_img" src={health}/>
+               <span className="category-list_link">Santé</span>
+            </Link>    
+            <Link to="/science" >
+                <img className="category-list_img" src={science}/>
+                <span className="category-list_link">Science</span>
+            </Link>    
+            <Link to="/technology" >
+                <img className="category-list_img" src={technology}/>
+               <span className="category-list_link">Technologie</span>
+            </Link> 
+        </div>  
     </div>
-    <div className="category-list_container">
-        <a href="#" class="category-list_item" id="btn-category">
-                <img className="category-list_img" src={health} />
-                <span className="category-list_link">Santé</span>
-        </a>
-    </div>
+
 </main>
 <Footer />
 </>
