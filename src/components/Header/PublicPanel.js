ae3bd254e9b582c8ca00b1cbb8e9392f09b7c617
Kevin CHALLIT <kevin.challit@gmail.com>
2021-11-28 15:08:30 +0100

    finish switcher form login /subscribe link

--- src/components/Header/PublicPanel.js
+++ src/components/Header/PublicPanel.js
@@ -13,7 +13,7 @@ const PublicPanel = ({ handleRequestState }) => {
     return (
         <>
         
-        <SubscribeLink to="/registration" className="user-subscribe_link" switchState={switchState}>Inscrivez-vous </SubscribeLink>
+        <Link to="/registration" className={`user-subscribe_link ${switchState ? '' : 'hide'}`}> Inscrivez-vous </Link>
         
         <FormLoginWrapper switchState={switchState}>
             <Login handleRequestState={handleRequestState} />
@@ -27,51 +27,13 @@ const PublicPanel = ({ handleRequestState }) => {
     )
 }
 
-export default PublicPanel
+export default PublicPanel;
 
 const FormLoginWrapper = styled.div`
     display: flex;
     padding-right: 10px;
-    @media (max-width: 1400px) {
-        width: auto; 
-    }
     @media (max-width: 900px) {
         display: ${props => props.switchState ? 'none' : 'flex'};
         justify-content: center;
     }
-    @media(max-width: 540px) {
-        display: ${props => props.switchState ? 'none' : 'flex'};
-        justify-content: center;
-    }
-`
-
-const SubscribeLink = styled(Link)`
-    text-decoration: none;
-    color: #ffffff;
-    font-size: 0.9rem;
-    text-transform: uppercase;
-    padding: 15px;
-    background: rgba(216, 58, 86, 0.85);
-    display: flex;
-    align-self: center;
-    justify-self: center;
-    transition: 0.5s;
-    border-radius: 5px;
-    font-family: Otomanopee One, sans-serif;
-    &:hover {
-        background: #215f00;
-        padding: 20px;
-    }
-    @media (max-width: 900px) {
-        display: ${props => props.switchState ? 'flex' : 'none'};
-    }
-    @media (max-width: 540px) {
-        padding: 10px;
-        font-size: 0.9rem;
-        &:hover{
-            padding: 15px;
-        }
-    }
-
-
 `
